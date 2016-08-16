acfeef64d26c19750f6c173803d7f45e348d8e2b
maurosaquino <mauro.s.aquino@gmail.com>
2016-08-16 20:25:24 -0300

    08/16/2016 - Commit inicial de GitGui en pagina Index

---
+++ Index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Aquino</title>
+</head>
+<body>
+	<h1>
+		Hola Mundo
+	</h1>
+
+	<?php
+
+		//codigo HTML embebido en el PHP
+		echo "<h2> Hola Php </h2>";
+
+		//declaracion de variables "$"
+		$numero = 10;
+
+		//numero y variable concatenado por "."
+		echo "A el numero es" . $numero;
+
+		//Numero y variable sin concatenar
+		echo "B el numero es  $numero";
+
+		//Numero y variable sin concatenar
+		echo 'C el numero es  $numero';
+
+	?>
+
+</body>
+</html>
