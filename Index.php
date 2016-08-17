93b9bcd0b75c176e7c185c8c424e6abd6c367cfd
Mauro Sebastian Aquino <mauro.aquino@globant.com>
2016-08-17 11:46:20 -0300

    17/08/2016 Agregado switch y case para el 7 que sea menor a 5%

--- Index.php
+++ Index.php
@@ -15,16 +15,74 @@
 		$par  	= 0;
 		$vector = array(0,0,0,0,0,0,0,0,0,0);
 		$vueltas = 0;
+		$muestra = 0;
 
 		
-		for ($vueltas; $vueltas < 10; $vueltas++) {
+		//Definir cantidad de vueltas
+
+		for ($vueltas; $vueltas < 10000; $vueltas++) {
 		
 			$random = rand(1,10);
 
-			echo "$random <br>";
+				
+			//Verificar si es par o impar
+
+			if ($random/2==0) {
+
+				$par++;
+			
+			} else {
+
+				$impar++;
+			}
+
+			//Acumular variables por numero
+
+			switch ($random) {
+			case 1: $vector[0]++;
+			break;
+			case 2: $vector[1]++;
+			break;
+			case 3: $vector[2]++;
+			break;
+			case 4: $vector[3]++;
+			break;
+			case 5: $vector[4]++;
+			break;
+			case 6: $vector[5]++;
+			break;
+			case 7: 
+				if ($vector[6] <= (5*$vueltas) / 100) {
+					continue;
+				} else {
+				
+						$vector[6]++;
+				}
+			break;
+			case 8: $vector[7]++;
+			break;
+			case 9: $vector[8]++;
+			break;
+			case 10: $vector[9]++;
+			break;
+			}
 
 		}
 
+		
+		
+		echo "Cantidad de uno:" . ($vector[0]*100) /$vueltas . "% <br>";
+		echo "Cantidad de dos:" . ($vector[1]*100) /$vueltas . " % <br>";
+		echo "Cantidad de tres:" . ($vector[2]*100) /$vueltas . " % <br>";
+		echo "Cantidad de cuatro:" . ($vector[3]*100) /$vueltas . " % <br>";
+		echo "Cantidad de cinco:" . ($vector[4]*100) /$vueltas . " % <br>";
+		echo "Cantidad de seis:" . ($vector[5]*100) /$vueltas . " % <br>";
+		echo "Cantidad de siete:" . ($vector[6]*100) /$vueltas . " % <br>";
+		echo "Cantidad de ocho:" . ($vector[7]*100) /$vueltas . " % <br>";
+		echo "Cantidad de nueve:" . ($vector[8]*100) /$vueltas . " % <br>";
+		echo "Cantidad de diez:" . ($vector[9]*100) /$vueltas . " % <br>";
+
+		
 
 	?>
 
