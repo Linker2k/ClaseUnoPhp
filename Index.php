22c11fc713624779411ad49b492c509153314257
Mauro Sebastian Aquino <mauro.aquino@globant.com>
2016-08-17 11:27:07 -0300

    17/08/2016 Agregado el primer For

--- Index.php
+++ Index.php
@@ -14,11 +14,16 @@
 		$impar 	= 0;
 		$par  	= 0;
 		$vector = array(0,0,0,0,0,0,0,0,0,0);
+		$vueltas = 0;
 
-		echo $vector[1];
+		
+		for ($vueltas; $vueltas < 10; $vueltas++) {
+		
+			$random = rand(1,10);
 
+			echo "$random <br>";
 
-
+		}
 
 
 	?>
