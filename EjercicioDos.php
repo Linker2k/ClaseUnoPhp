d4338761ec58490c402357c1c2278bc6a7e48562
maurosaquino <mauro.s.aquino@gmail.com>
2016-08-23 19:31:26 -0300

    23/08/2016 - Ejercicio dos (a terminar lo de la estaciòn)

---
+++ EjercicioDos.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Ejercicio Dos - Mauro Aquino</title>
+</head>
+<body>
+
+<?php
+
+$fecha = date('D d - F - Y');
+$fechados = date('d - n - y');
+$fechatres = date('j - M - Y');
+$diaano = date('z'); 
+
+echo "Formato Uno: $fecha <br>";
+echo "Formato Dos: $fechados <br>";
+echo "Formato Tres: $fechatres <br>";
+
+?>
+
+</body>
+</html>
